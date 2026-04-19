4e92f6ffdc1ecf7c720ef5632f045797aa9a36c7
Md Muhibul Islam Abir <muhibulislamabir@gmail.com>
2026-04-19 21:20:51 +0600

    simplifying code

--- src/ui/Dashboard.java
+++ src/ui/Dashboard.java
@@ -1,10 +1,8 @@
 package ui;
-
+import patterns.NavigationFacade;
+import patterns.UIFactory;
 import java.awt.*;
 import javax.swing.*;
-import uiPatterns.NavigationFacade;
-import uiPatterns.UIFactory;
-
 public class Dashboard extends JFrame {
     private static Dashboard instance;
     private NavigationFacade nav;
@@ -23,7 +21,6 @@ public class Dashboard extends JFrame {
         contentPanel.add(new BloodStockPanel(),        "STOCK_LIST");
         contentPanel.add(new BloodRequestPanel(),      "BLOOD_REQUEST");
         contentPanel.add(new DonorListPanel(),         "DONOR_LIST");
-        contentPanel.add(new SettingsPanel(),          "SETTINGS");
 
         NavigationFacade nav = new NavigationFacade(contentPanel, cardLayout);
 
@@ -46,16 +43,14 @@ public class Dashboard extends JFrame {
         JButton btnDonors = UIFactory.createMenuButton("View Donors");
         btnDonors.addActionListener(e -> nav.openDonorList());
         
-        // --- NEW MENU BUTTON ---
-        JButton btnSettings = UIFactory.createMenuButton("Settings");
-        btnSettings.addActionListener(e -> nav.openSettings());
+
 
         sidebar.add(btnHome);
         sidebar.add(btnReg);
         sidebar.add(btnStock);
         sidebar.add(btnRequest);
         sidebar.add(btnDonors);
-        sidebar.add(btnSettings);
+
 
         add(sidebar, BorderLayout.WEST);
         add(contentPanel, BorderLayout.CENTER);
